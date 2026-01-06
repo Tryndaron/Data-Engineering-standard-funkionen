564c1ca5753254b003f5dea6f6286a32f5a1f62e
tryndaron <aaron.roggenland@gmx.net>
2026-01-06 15:42:15 +0100

    added setup so it can be used as a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="PandasDFA",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",  # falls du Abhängigkeiten hast
+    ],
+)
